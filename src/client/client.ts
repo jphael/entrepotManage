2c1643883c6fba5b0baf36d1fb60031e80dd66a5
Phael <jeanphael@dataconsulting.tech>
2022-09-06 09:45:19 +0400

    push version

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,25 +1,24 @@
-
-import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
-import { LoadingManager } from "three/src/loaders/LoadingManager"
-import Stats from "three/examples/jsm/libs/stats.module"
-import { CustomMesh } from "./CustomMesh"
-import { MeshModel } from "./MeshModel"
-import { DataResponse } from "./DataResponse"
-import * as $ from "jquery"
-import * as bootstrap from "bootstrap"
-import { Mesh } from "three"
-import { Vector3 } from "three/src/math/Vector3"
-var myModal : bootstrap.Modal
-var currentSelected: Mesh
-var listRack: Array<CustomMesh>
-var listIdCheckBox : string[] = new Array()
-listRack = new Array()
-var listRackFromDB: Array<MeshModel>
-const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
-scene.background = new THREE.Color(0xccffff)
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { LoadingManager } from "three/src/loaders/LoadingManager";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { CustomMesh } from "./CustomMesh";
+import { MeshModel } from "./MeshModel";
+import { DataResponse } from "./DataResponse";
+import * as $ from "jquery";
+import * as bootstrap from "bootstrap";
+import { Mesh } from "three";
+import { Vector3 } from "three/src/math/Vector3";
+var myModal: bootstrap.Modal;
+var currentSelected: Mesh;
+var listRack: Array<CustomMesh>;
+var listIdCheckBox: string[] = new Array();
+listRack = new Array();
+var listRackFromDB: Array<MeshModel>;
+const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
+scene.background = new THREE.Color(0xccffff);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -62,41 +61,39 @@ const coneGeometry = new THREE.ConeGeometry(0.05, 0.2, 8);
 const raycaster = new THREE.Raycaster();
 const sceneMeshes: THREE.Object3D[] = [];
 let intersectedObject: THREE.Object3D | null;
-const manager = new LoadingManager()
+const manager = new LoadingManager();
 const loader = new GLTFLoader(manager);
-manager.itemStart('final')
+manager.itemStart("final");
 loader.load(
-  "models/final.glb", 
+  "models/final.glb",
   function (gltf) {
     gltf.scene.traverse(function (child) {
       if ((child as THREE.Mesh).isMesh) {
         const m = child as THREE.Mesh;
         m.receiveShadow = true;
         m.castShadow = true;
-        //console.log('--'+m.name);
+        //console.log("" + m.name);
         (m.material as THREE.MeshStandardMaterial).flatShading = true;
         //m.material = new THREE.MeshStandardMaterial({color: 0xffffff * Math.random()});
+
         if (m.name.includes("PL_")) {
-          console.log(m.name);
+          console.log('newName---'+m.name);
           //rouge
           //m.material = new THREE.MeshStandardMaterial({color: 0xFF0000});
-          //console.log('hitany ilay batiment-----'+m.name);
-          var tempCustomMesh = new CustomMesh(m);
-          listRack.push(tempCustomMesh);
+            var tempCustomMesh = new CustomMesh(m);
+            listRack.push(tempCustomMesh);
+            //console.log('-- taille palette '+listRack.length);
           //m.visible = false;
-        }
-         else if (m.name.includes("CR_")) {
+        } else if (m.name.includes("CR_")) {
           //console.log('--'+m.name);
-          var splitted = m.name.split("_",2);
-          var paleteNameToFind = 'PL_'+splitted[1]+'_1'
-          //console.log('tofind--'+paleteNameToFind);
+          var splitted = m.name.split("_", 2);
+          var paleteNameToFind = "PL_" + splitted[1];
           listRack.forEach((o: CustomMesh, i) => {
             if (paleteNameToFind === o.mesh.name) {
               //console.log('nahita--');
-              o.setCarton(m)
+              o.setCarton(m);
             }
           });
-          //console.log(m.name);
         }
         sceneMeshes.push(m);
       }
@@ -107,28 +104,21 @@ loader.load(
         l.shadow.mapSize.width = 2048;
         l.shadow.mapSize.height = 2048;
       }
-      manager.itemEnd( 'final' );
+      manager.itemEnd("final");
     });
     scene.add(gltf.scene);
   },
   (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    
+    //console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
   },
   (error) => {
     console.log(error);
-  },
-  
+  }
 );
 
 manager.onLoad = function () {
-  console.log('---------------everything is done---------------');
-  //getList()
-  listRack.forEach((o: CustomMesh, i) => {
-    console.log(o.mesh.name+'--'+o.carton?.name);
-  });
-  
-  
+  //console.log("---------------everything is done-- taille palette " + listRack.length);
+  getList();
 };
 
 window.addEventListener("resize", onWindowResize, false);
@@ -148,26 +138,27 @@ function onMouseMove(event: MouseEvent) {
     y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1,
   };
 
-
   raycaster.setFromCamera(mouse, camera);
 
   const intersects = raycaster.intersectObjects(sceneMeshes, false);
-  
 
   if (intersects.length > 0) {
     intersectedObject = intersects[0].object;
-    console.log('name clicked '+intersectedObject.name);
+    console.log("name clicked " + intersectedObject.name);
   } else {
     intersectedObject = null;
   }
   //if(!intersectedObject || !(intersectedObject.name.includes("PL_")) || !(intersectedObject.name.includes("CR_"))) return
   listRack.forEach((o: CustomMesh, i) => {
-    if ((intersectedObject && intersectedObject.name === o.mesh.name) || (intersectedObject && intersectedObject.name === o.carton?.name)) {
-      paletteByRack()
+    if (
+      (intersectedObject && intersectedObject.name === o.mesh.name) ||
+      (intersectedObject && intersectedObject.name === o.carton?.name)
+    ) {
+      paletteByRack();
       //$("#myIdToShow").hide();
-      //o.updateColorMaterial();
+      o.updateColorMaterial();
       if (o.isSelected) {
-       // positionClicked(intersectedObject.position, o);
+        // positionClicked(intersectedObject.position, o);
       }
     }
   });
@@ -182,16 +173,15 @@ function positionClicked(position: Vector3, o: CustomMesh) {
   var top = (height * (-v.y + 1)) / 2;
 
   $("#myIdToShow").css({ left: left, top: top });
-  console.log(o.mesh.name);
+  //console.log(o.mesh.name);
   $(".meshe-name").text(o.mesh.name);
-  $(".meshe-place").text("Etage : "+o.etage);
-  $(".meshe-price").text("rack : "+o.rack);
+  $(".meshe-place").text("Etage : " + o.etage);
+  $(".meshe-price").text("rack : " + o.rack);
   $("#myIdToShow").show();
-  
 }
 
 //($('#myModal') as any ).show()
-//(<any>$('#myModal')).modal('show'); 
+//(<any>$('#myModal')).modal('show');
 var hemiLight = new THREE.HemisphereLight(0xffffff, 0x0000ff);
 hemiLight.position.set(0, 300, 0);
 scene.add(hemiLight);
@@ -225,140 +215,147 @@ function render() {
 animate();
 
 $("#btnSend").on("click", (event: JQuery.Event) => {
-  
-  var idPaletteSelected : string[]  = new Array()
-  listIdCheckBox.forEach((idPalette:string) => {
-  
-    if($('#' + idPalette).is(":checked")) {
-      
-      idPaletteSelected.push(idPalette)
+  var idPaletteSelected: string[] = new Array();
+  listIdCheckBox.forEach((idPalette: string) => {
+    if ($("#" + idPalette).is(":checked")) {
+      idPaletteSelected.push(idPalette);
       listRack.forEach((o: CustomMesh, i) => {
-        if (intersectedObject && intersectedObject.name === o.mesh.name) {
-          o.updateColorMaterial();
+        if (idPalette && idPalette === o.mesh.name) {
+          //console.log("---------manao update--" + o.mesh.name);
+          //o.updateColorMaterial();
         }
       });
     }
-    
-
-
   });
-  console.log("---------list palette selectionner--"+ idPaletteSelected)
-  myModal.hide()
+  //console.log("---------list palette  params--" + JSON.stringify(idPaletteSelected));
+  myModal.hide();
+  updateStatus(idPaletteSelected);
 });
 
-function resetAllData(){
-  listIdCheckBox = new Array()
-  $("#wsresult").html("")
-
-}
-async function paletteByRack() {
-  resetAllData()
+async function updateStatus(params : string[]) {
   try {
-    const response = await fetch('http://127.0.0.1:8003/palette-rack', {
-      method: 'post',
-      headers: { 
-        accept: 'application/json',
-      }, 
-      body:new URLSearchParams("rack=AAA1&colonne=1")
-    }); 
- 
+    var idArray = JSON.stringify(params)
+    const response = await fetch(
+      "http://127.0.0.1:8003/palette-update-status",
+      {
+        method: "post",
+        headers: {
+          accept: "application/json",
+        },
+        //body: `{idArray["PL_A0001E3C4","PL_A0001E2C4","PL_A0001E1C4"]}`
+        body: new URLSearchParams(`idArray=${idArray}`),
+        //body: JSON.stringify("idArray":idPaletteSelected),
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Error! status: ${response.status}`);
     }
-
-    const result = await response.json()
-
-    //console.log('result ws--------'+JSON.stringify(result));
-   let obj: DataResponse = JSON.parse(JSON.stringify(result));
-   var listPaletteByRack: Array<MeshModel>;
-   listPaletteByRack = obj.list;
-   const myMap: Map<number, [MeshModel]> = new Map();
- 
- listPaletteByRack.forEach((itemModel: MeshModel, j) => {
-  if(myMap.get(itemModel.etage)  && myMap.has(itemModel.etage)){
-    var currentList = myMap.get(itemModel.etage)
-    //currentList?.push("Etage "+itemModel.etage + " colonne "+itemModel.palette_position)
-    currentList?.push(itemModel)
-  }else{
-    myMap.set(itemModel.etage,[itemModel])
-  }
-});
-
-var divPalette =  $("#wsresult")
-myMap.forEach((value: [MeshModel], key: number) => {
-  //console.log(key, value);
-  var divEtage = `<div class='mb-3 row'>`
-  divPalette.append(divEtage)
-  value.forEach((curentValue:MeshModel) => {
-    //console.log(curentValue);
-    var name : string  = curentValue.name
-    var palettePosition = curentValue.palette_position
-    var paleteEtage = curentValue.etage
-    var classColMD3  = `class='col-md-3'`
-    var strPalettePosition = `Etage ${paleteEtage}  colonne ${palettePosition}` 
-    var enableColor = `accent-color: green`
-    var disableColor = `accent-color: red`
-    var checkDiv = ''
-    if(curentValue.is_reserved)
-    {
-       checkDiv = `<div ${classColMD3}><input disabled type='checkbox' id=${name}  name=${name} /><label for=${name}>${strPalettePosition} </label></div>`
-    }
-    else{
-      listIdCheckBox.push(name)
-       checkDiv = `<div ${classColMD3}><input  type='checkbox' id=${name}  name=${name} /><label for=${name}>${strPalettePosition} </label></div>`
-    }
-    divPalette.append(checkDiv)
-  });
-  var divEtageEnd = "</div>"
-  divPalette.append(divEtageEnd)
-
-})
-
-  myModal = new bootstrap.Modal("#myModal", {});
-  //$('#category').val("testaaaaaaaaaaa");
-  myModal.show()
-  } catch (err) {
-    console.log(err)
-  }
-}
- 
-async function getList() {
-  try {
-    const response = await fetch('http://127.0.0.1:8003/listPalette', {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-      },
-    });
- 
-
-    if (!response.ok) {
-      throw new Error(`Error! status: ${response.status}`);
-    }
-
-    const result = await response.json()
-    //const itemListResponse: MeshModel[] = <MeshModel[]>JSON.parse(result);
-
-    //console.log('result--------'+JSON.stringify(result));
-   // console.log
-   let obj: DataResponse = JSON.parse(JSON.stringify(result));
-   listRackFromDB = obj.list;
-   getListeReservedFromDB()
   } catch (err) {
     console.log(err);
   }
 }
 
+function resetAllData() {
+  listIdCheckBox = new Array();
+  $("#wsresult").html("");
+}
+async function paletteByRack() {
+  resetAllData();
+  try {
+    const response = await fetch("http://127.0.0.1:8003/palette-rack", {
+      method: "post",
+      headers: {
+        accept: "application/json",
+      },
+      body: new URLSearchParams("rack=AAA1&colonne=1"),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    let obj: DataResponse = JSON.parse(JSON.stringify(result));
+    var listPaletteByRack: Array<MeshModel>;
+    listPaletteByRack = obj.list;
+    const myMap: Map<number, [MeshModel]> = new Map();
+
+    listPaletteByRack.forEach((itemModel: MeshModel, j) => {
+      if (myMap.get(itemModel.etage) && myMap.has(itemModel.etage)) {
+        var currentList = myMap.get(itemModel.etage);
+        //currentList?.push("Etage "+itemModel.etage + " colonne "+itemModel.palette_position)
+        currentList?.push(itemModel);
+      } else {
+        myMap.set(itemModel.etage, [itemModel]);
+      }
+    });
+
+    var divPalette = $("#wsresult");
+    myMap.forEach((value: [MeshModel], key: number) => {
+      var divEtage = `<div class='mb-3 row'>`;
+      divPalette.append(divEtage);
+      value.forEach((curentValue: MeshModel) => {
+        var name: string = curentValue.name;
+        var palettePosition = curentValue.palette_position;
+        var paleteEtage = curentValue.etage;
+        var classColMD3 = `class='col-md-3'`;
+        var strPalettePosition = `Etage ${paleteEtage}  position ${palettePosition}`;
+        var enableColor = `accent-color: green`;
+        var disableColor = `accent-color: red`;
+        var checkDiv = "";
+        if (curentValue.is_reserved) {
+          checkDiv = `<div ${classColMD3}><input disabled type='checkbox' id=${name}  name=${name} /><label for=${name}>${strPalettePosition} </label></div>`;
+        } else {
+          listIdCheckBox.push(name);
+          checkDiv = `<div ${classColMD3}><input  type='checkbox' id=${name}  name=${name} /><label for=${name}>${strPalettePosition} </label></div>`;
+        }
+        divPalette.append(checkDiv);
+      });
+      var divEtageEnd = "</div>";
+      divPalette.append(divEtageEnd);
+    });
+
+    myModal = new bootstrap.Modal("#myModal", {});
+    //$('#category').val("testaaaaaaaaaaa");
+    myModal.show();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getList() {
+  try {
+    const response = await fetch("http://127.0.0.1:8003/listPalette", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    //const itemListResponse: MeshModel[] = <MeshModel[]>JSON.parse(result);
+
+    //console.log('result--------'+JSON.stringify(result));
+    let obj: DataResponse = JSON.parse(JSON.stringify(result));
+    listRackFromDB = obj.list;
+    getListeReservedFromDB();
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 function getListeReservedFromDB() {
   //recuperation de la liste dans la base
   listRackFromDB.forEach((o: MeshModel, i) => {
-    //console.log('boucle-----'+o.name)
     //var result = listRack.filter(e => e.mesh.name === 'Palette_B3503')
     var result = listRack.filter((e) => e.mesh.name === o.name);
     if (result.length > 0) {
-      //console.log('nahita-----'+o.name)
       //result[0].mesh.material = new THREE.MeshStandardMaterial({color: 0xDDE70D})
       //result[0].setInfoFromDB(o)
     }
